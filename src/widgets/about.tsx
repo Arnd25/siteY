4a4c7e8e7b2c9d0f8fe2ae29bb047e6d87dcba7e
Arnd25 <And30206@gmail.com>
2025-12-18 16:22:06 +0300

    Update about.tsx

--- src/widgets/about.tsx
+++ src/widgets/about.tsx
@@ -6,7 +6,7 @@ const About = () => {
         <section id="About" className="text-black pt-24">
             <Container className="flex flex-col md:flex-row gap-10 md:gap-30 px-5 justify-center">
                 <h2 className="text-4xl whitespace-nowrap self-center-safe" >О Компании</h2>
-                <p className="max-w-5xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias at cupiditate distinctio dolorem earum ex facilis id ipsa nobis obcaecati quis rem soluta ut, voluptatem. Adipisci aliquam aspernatur, blanditiis commodi culpa debitis deserunt doloribus eaque illo inventore ipsam iusto maxime necessitatibus nemo nostrum odio, perferendis, porro quia quo rerum. Adipisci at ea error harum in magnam molestiae nulla perspiciatis qui soluta. Ducimus, molestiae, obcaecati!
+                <p className="max-w-5xl">Мы создаём масштабируемые цифровые продукты с нуля и модернизируем существующие решения. Внедряем интеллектуальные алгоритмы, оптимизируем архитектуру и производительность, чтобы ваш бизнес работал эффективнее, быстрее и готов был к будущему.
                 </p>
             </Container>
         </section>
