24861b43d0b37a4ee5bf7eae624591fc28e37e12
Arnd25 <And30206@gmail.com>
2025-12-22 15:27:24 +0300

    Update projects.tsx

--- src/widgets/projects.tsx
+++ src/widgets/projects.tsx
@@ -82,7 +82,7 @@ const Projects = ({ page = 'notmain' }: { page?: string }) => {
                                         alt={""}
                                         width={214}
                                         height={214}
-                                        className="object-cover rounded-2xl border-2 border-gray-300 "/>
+                                        className="object-cover w-full h-full rounded-2xl border-2 border-gray-300 "/>
                                     <div className="absolute top-0 left-0 p-4">
                                         <h3 className="sm:text-2xl font-bold text-lg">{ProjectAttributes.title}</h3>
 
