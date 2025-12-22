89f512a3c08ffc87afa5b3f53e960e01b5ea3825
Arnd25 <And30206@gmail.com>
2025-12-22 22:49:22 +0300

    Update hero.tsx

--- src/widgets/hero.tsx
+++ src/widgets/hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
     return (            
         <div className="w-full bg-[url(@/../public/static/hero_bg.png)] bg-cover bg-center bg-no-repeat">
             <Container className="py-40 md:py-52 text-black flex flex-col gap-24 w-full px-5 xl:px-0">
-                <h1 className="text-3xl md:text-6xl text-white font-bold"> Высоконагрузочные <span className="text-blue-600">решения</span> для <span className="text-blue-800">амбициозного роста</span></h1>
+                <h1 className="text-2xl  sm:text-3xl md:text-6xl text-white font-bold"> Высоконагрузочные <span className="text-blue-400">решения</span> для <span className="text-blue-300">амбициозного роста</span></h1>
                 <div className="flex flex-col sm:flex-row lg:items-center gap-5 xl:gap-48">
                     <p className="max-w-lg text-sm sm:text-lg text-gray-100 font-medium">Фокусируемся на архитектуре, которая обеспечивает стабильность сегодня и бесконечное масштабирование завтра.</p>
                     <Button variant={"link"} className={"hover:no-underline text-lg border-cyan-300 border-2 py-8 px-5 rounded-2xl md:text-2xl text-cyan-300"} >Написать в telegram <FaTelegramPlane size={20} /> </Button>
